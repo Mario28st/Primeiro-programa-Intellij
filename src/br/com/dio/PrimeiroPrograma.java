8d018b88c8723c88f19b9f68d80d8e3eb41f83d3
Mario28st <mario28st@gmail.com>
2022-02-16 11:27:29 -0300

    Atualização nova classe

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,5 +4,9 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         System.out.println("Primeiro Programa Intellij");
+
+        Gato gato = new Gato ("Elvis", "MArrom", 5);
+
+        System.out.println(gato);
     }
 }
